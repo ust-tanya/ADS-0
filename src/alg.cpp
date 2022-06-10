4700f9a0367a6b4db12b2d3d93c06918e30bde57
ust-tanya <94698360+ust-tanya@users.noreply.github.com>
2022-06-10 21:47:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,10 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    if (a == b)
+        return a;
+    if (a > b)
+        return gcd(a-b, b);
+    else
+        return gcd(a, b-a);
 }
